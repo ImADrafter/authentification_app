728c2e3347b17fc9076427d0716aa515646f206f
Glitch (lovely-danger) <none>
2020-01-11 19:16:48 +0000

    👶⛹ Checkpoint ./package.json:2900523/5066 ./server.js:2900523/2070 ./views/pug/index.pug:2900523/7

--- server.js
+++ server.js
@@ -3,15 +3,29 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const session = require("express-session");
+const passport = require("passport");
+const customEnv = require("custom-env");
 
 const app = express();
 
 app.set("view engine", "pug");
+app.use(passport.initialize())
+app.use(passport.session());
 
 fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+customEnv.env();
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
 app.route("/").get((req, res) => {
   res.render(process.cwd() + "/views/pug/index.pug", {
