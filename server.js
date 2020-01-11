6d1b658ab7e5cd2ba41b02f6e4b5eb26d9ca8b82
Glitch (lovely-danger) <none>
2020-01-11 19:40:43 +0000

    🔑🚗 Checkpoint ./server.js:2900523/7681 ./package.json:2900523/1810

--- server.js
+++ server.js
@@ -7,23 +7,43 @@ const session = require("express-session");
 const passport = require("passport");
 const customEnv = require("custom-env");
 const db = require("mongodb");
-const ObjectID = require('mongodb').ObjectID;
-const mongo = require('mongodb').MongoClient;
+const ObjectID = require("mongodb").ObjectID;
+const mongo = require("mongodb").MongoClient;
+const LocalStrategy = require("passport-local");
 
 const app = express();
 
 mongo.connect(process.env.DATABASE, (err, db) => {
-  if(err) {
-    console.log('Database error: ' + err);
+  if (err) {
+    console.log("Database error: " + err);
   } else {
-    console.log('Successful database connection');
-
+    console.log("Successful database connection");
+    passport.use(
+      new LocalStrategy(function(username, password, done) {
+        db.collection("users").findOne({ username: username }, function(
+          err,
+          user
+        ) {
+          console.log("User " + username + " attempted to log in.");
+          if (err) {
+            return done(err);
+          }
+          if (!user) {
+            return done(null, false);
+          }
+          if (password !== user.password) {
+            return done(null, false);
+          }
+          return done(null, user);
+        });
+      })
+    );
     //serialization and app.listen
   }
 });
 
 app.set("view engine", "pug");
-app.use(passport.initialize())
+app.use(passport.initialize());
 app.use(passport.session());
 
 fccTesting(app); //For FCC testing purposes
@@ -44,15 +64,10 @@ passport.serializeUser((user, done) => {
   done(null, user._id);
 });
 passport.deserializeUser((id, done) => {
-//  db.collection('users').findOne(
-//    {_id: new ObjectID(id)},
-//      (err, doc) => {
-//        done(null, doc);
-//      }
-//  );
-  done(null, null);
+  db.collection("users").findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, doc);
+  });
 });
-
 
 app.route("/").get((req, res) => {
   res.render(process.cwd() + "/views/pug/index.pug", {
