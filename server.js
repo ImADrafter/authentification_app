5d8142fe92d3bb9e0f15dbb0d38dcd95eca4383f
Glitch (lovely-danger) <none>
2020-01-12 19:32:56 +0000

    🍘🍖 Checkpoint ./server.js:2900523/55051

--- server.js
+++ server.js
@@ -68,67 +68,58 @@ mongo.connect(process.env.DATABASE, (err, client) => {
       })
     );
   }
-});
 
-app.route("/").get((req, res) => {
-  res.render(process.cwd() + "/views/pug/index.pug", {
-    title: "Home page",
-    message: "Please login",
-    showLogin: true,
-    showRegistration: true
+  app.route("/").get((req, res) => {
+    res.render(process.cwd() + "/views/pug/index.pug", {
+      title: "Home page",
+      message: "Please login",
+      showLogin: true,
+      showRegistration: true
+    });
   });
-});
 
-app
-  .route("/login")
-  .post(
-    passport.authenticate("local", { failureRedirect: "/" }),
-    (req, res) => {
-      res.redirect("/profile");
+  app
+    .route("/login")
+    .post(
+      passport.authenticate("local", { failureRedirect: "/" }),
+      (req, res) => {
+        res.redirect("/profile");
+      }
+    );
+
+  // Check if user is authenticated on /profile request.
+  function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
     }
-  );
-
-// Check if user is authenticated on /profile request.
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
+    res.redirect("/");
   }
-  res.redirect("/");
-}
 
-// Unauthenticate and logout
-app.route("/logout").get((req, res) => {
-  req.logout();
-  res.redirect("/");
-});
-
-app.route("/profile").get(ensureAuthenticated, (req, res) => {
-  res.render(process.cwd() + "/views/pug/profile", {
-    username: req.user.username
+  // Unauthenticate and logout
+  app.route("/logout").get((req, res) => {
+    req.logout();
+    res.redirect("/");
   });
-});
 
-// Handle 404
-app.use((req, res, next) => {
-  res
-    .status(404)
-    .type("text")
-    .send("Not found");
-});
+  app.route("/profile").get(ensureAuthenticated, (req, res) => {
+    res.render(process.cwd() + "/views/pug/profile", {
+      username: req.user.username
+    });
+  });
 
-// Registration
-
-app.route("/register").post(
-  (req, res, next) => {
-    mongo
-      .collection("users")
-      .findOne({ username: req.body.username }, function(err, user) {
+  // Registration
+  app.route("/register").post(
+    (req, res, next) => {
+      db.collection("users").findOne({ username: req.body.username }, function(
+        err,
+        user
+      ) {
         if (err) {
           next(err);
         } else if (user) {
           res.redirect("/");
         } else {
-          mongo.collection("users").insertOne(
+          db.collection("users").insertOne(
             {
               username: req.body.username,
               password: req.body.password
@@ -143,13 +134,22 @@ app.route("/register").post(
           );
         }
       });
-  },
-  passport.authenticate("local", { failureRedirect: "/" }),
-  (req, res, next) => {
-    res.redirect("/profile");
-  }
-);
+    },
+    passport.authenticate("local", { failureRedirect: "/" }),
+    (req, res, next) => {
+      res.redirect("/profile");
+    }
+  );
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
+  // Handle 404
+  app.use((req, res, next) => {
+    res
+      .status(404)
+      .type("text")
+      .send("Not found");
+  });
+
+  app.listen(process.env.PORT || 3000, () => {
+    console.log("Listening on port " + process.env.PORT);
+  });
 });
